2eaa08c709ea181d9fcec809d3eace9495a9d4cd
Arun Thekkuden <arun.thekkuden14@gmail.com>
2017-05-22 21:21:03 +0530

    Updated settings

--- src/everest/settings/settings.py
+++ src/everest/settings/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'everest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'everest',
+        'HOST': 'localhost',
+        'USER': 'everest',
+        'PASSWORD': 'everest',
+        'PORT': '5432',
+    },
 }
 
 
