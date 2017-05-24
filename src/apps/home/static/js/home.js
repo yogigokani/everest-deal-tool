07ecd4d9c6af88f77c32ee63ba8ed3eab7ae00fd
yogigokani <yogigokani@rediffmail.com>
2017-05-24 10:51:19 +0530

    Bet project Everest - autocomplete feature, homepage

--- src/apps/home/static/js/home.js
+++ src/apps/home/static/js/home.js
@@ -1,31 +1,49 @@
 $(document).ready(function(){
-    $( function() {
-        var availableTags = [
-            "ActionScript",
-            "AppleScript",
-            "Asp",
-            "BASIC",
-            "C",
-            "C++",
-            "Clojure",
-            "COBOL",
-            "ColdFusion",
-            "Erlang",
-            "Fortran",
-            "Groovy",
-            "Haskell",
-            "Java",
-            "JavaScript",
-            "Lisp",
-            "Perl",
-            "PHP",
-            "Python",
-            "Ruby",
-            "Scala",
-            "Scheme"
-        ];
-        $( "#tags" ).autocomplete({
-          source: availableTags
-        });
+    var projects = [
+        {
+            showString: "yogi",
+            hiddenString: "SM",
+            url: "url"
+        },
+        {
+            showString: "motu",
+            hiddenString: "SM1",
+            url: "url1"
+        },
+        {
+            showString: "chotu",
+            hiddenString: "SM2",
+            url: "url2"
+        }
+    ];
+
+    $("#tags").autocomplete({
+
+        source: function(request, response){
+
+            var matcher = new RegExp( $.ui.autocomplete.escapeRegex( request.term ), "i" );
+
+            response( $.grep( projects, function( value ) {
+
+                // mention the fields to be searched in
+                return matcher.test(value.showString) || matcher.test(value.hiddenString) || matcher.test(value.url);
+
+            }));
+        }
+
     });
-})
+
+    // overridden renderItem of Jquery - autocomplete feature
+    $.ui.autocomplete.prototype._renderItem = function (ul, item) {
+
+        var createA = document.createElement('a');
+        var createAText = document.createTextNode(item.showString);
+        createA.setAttribute('href', item.url);
+        createA.appendChild(createAText);
+
+        return $( "<li class='abc'>" )
+            .append( createA )
+            .appendTo( ul );
+    };
+
+});
